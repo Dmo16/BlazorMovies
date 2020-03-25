fa3735c13b0d363fb4b6380c5424a449de5116c3
Dmo16 <david.morin16@gmail.com>
2020-03-24 19:52:50 -0500

    Finished adding movie posters

--- Client/Helpers/RepositoryInMemory.cs
+++ Client/Helpers/RepositoryInMemory.cs
@@ -13,9 +13,21 @@ namespace BlazorMovies.Client.Helpers
         {
             return new List<Movie>()
                 {
-                    new Movie(){Title = "Spider-Man: Far From Home", ReleaseDate = new DateTime(2019, 7, 2)},
-                    new Movie(){Title = "Moana", ReleaseDate = new DateTime(2016, 11, 23)},
-                    new Movie() {Title = "Inception", ReleaseDate = new DateTime(2010, 7, 16)}
+                    new Movie(){
+                        Title = "Spider-Man: Far From Home",
+                        ReleaseDate = new DateTime(2019, 7, 2),
+                        Poster = "https://www.movieartarena.com/imgs/spidermanffhz.jpg"
+                    },
+                    new Movie(){
+                        Title = "Moana",
+                        ReleaseDate = new DateTime(2016, 11, 23),
+                        Poster ="https://www.arthipo.com/image/cache/catalog/genel-tasarim/all-posters/sinema-cinema-film-postersleri/yabanci-filmler/1/pfilm1042-moana-26hwy6-film-movie-posters-cinema-kanvas-tablo-canvas-1000x1000.jpg"
+                    },
+                    new Movie() {
+                        Title = "Inception", 
+                        ReleaseDate = new DateTime(2010, 7, 16),
+                        Poster="https://posteritati.com/posters/000/000/057/626/inception-md-web.jpg"
+                    }
                 };
         }
     }
--- Shared/Entities/Movie.cs
+++ Shared/Entities/Movie.cs
@@ -7,13 +7,22 @@ namespace BlazorMovies.Shared.Entities
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         public string Poster { get; set; }
-        public string TitleBrief  { 
+        public string TitleBrief  {
             get
             {
                 if (string.IsNullOrEmpty(Title))
                 {
-
+                    return null;
                 }
-            }; set; }
+                if (Title.Length > 60)
+                {
+                    return Title.Substring(0, 60) + "...";
+                }
+                else
+                {
+                    return Title;
+                }
+            }
+        }
     }
 }
